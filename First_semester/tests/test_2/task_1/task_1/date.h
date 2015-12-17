fb15e6c443088a9c9092969bb26047a3149fc34c
AlbertMukhammadiev <beehead02@gmail.com>
2015-12-17 16:43:33 +0300

    output the greatest date

---
+++ First_semester/tests/test_2/task_1/task_1/date.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Date;
+
+Date *createDate();
+
+void fillingDate(Date *date, int numberOfField, int buffer);
+
+void comparingDates(Date *maxDate, Date *newDate);
+
+void printDate(Date *date);
