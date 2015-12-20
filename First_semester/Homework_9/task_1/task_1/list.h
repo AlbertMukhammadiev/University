8f801a1ca69c7db2edf7ed5f65b6df6b124eb09f
AlbertMukhammadiev <beehead02@gmail.com>
2015-12-20 16:03:52 +0300

    onset

---
+++ First_semester/Homework_9/task_1/task_1/list.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#pragma once
+
+struct List;
+
+struct ListElement;
+
+typedef ListElement* Position;
+
+typedef char Value;
+
+List *createList();
+
+void addNewElement(Value value, List *list);
+
+void deleteElement(Value value, List *list);
+
+void printList(List *list);
+
+void deleteList(List *p);
---
+++ First_semester/Homework_9/task_1/task_1/task_1.cpp
@@ -0,0 +1,15 @@
+// task_1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    
+	return EXIT_SUCCESS;
+}
+
