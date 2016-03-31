0bf0693bdbb781651ef0df16dd9a49129382756c
AlbertMukhammadiev <beehead02@gmail.com>
2016-03-31 19:50:13 +0300

    Map(using λ-function)

---
+++ Second_semestr/Homework_5/Map/Map.Tests/MapTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgramNamespace;
+
+namespace MapTests
+{
+    [TestClass]
+    public class MapTest
+    {
+        List<int> list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int>() { 1, 3, 2, 4};
+        }
+
+
+        [TestMethod]
+        public void MapOrderTest()
+        {
+            Program.Map(list, element => element * 3);
+            Assert.IsTrue(list[1] > list[2]);
+            Assert.IsTrue(list[0] < list[1]);
+            Assert.IsTrue(list[2] < list[3]);
+        }
+
+        [TestMethod]
+        public void MapSqrTest()
+        {
+            Program.Map(list, element => element * element);
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(9, list[1]);
+        }
+    }
+}
