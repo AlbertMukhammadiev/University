12374d23a91438b249a9395b2af459409df258a7
AlbertMukhammadiev <beehead02@gmail.com>
2016-04-16 14:54:17 +0300

    Filter (using λ-function)

---
+++ Second_semestr/Homework_5/Filter/Filter.Tests/FilterTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgramNamespace;
+
+namespace FilterTests
+{
+    [TestClass]
+    public class FilterTest
+    {
+        List<int> list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int>() { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+        }
+
+        [TestMethod]
+        public void EvenNumbersTest()
+        {
+            var listOfEvenNumbers = Program.Filter(list, element => element % 2 == 0);
+            foreach(var element in listOfEvenNumbers)
+            {
+                Assert.IsTrue(element % 2 == 0);
+            }
+        }
+
+        [TestMethod]
+        public void PositiveNumbersTest()
+        {
+            var listOfEvenNumbers = Program.Filter(list, element => element > 0);
+            foreach (var element in listOfEvenNumbers)
+            {
+                Assert.IsTrue(element > 0);
+            }
+        }
+    }
+}
