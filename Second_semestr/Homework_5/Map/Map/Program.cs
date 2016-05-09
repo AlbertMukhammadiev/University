96be9420b12f89b39df81d70aacbd6980c94f3b7
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-09 16:02:42 +0300

    Map (using λ-function)
    changed the type of the return value of Map and changed the comment to
    the function

--- Second_semestr/Homework_5/Map/Map/Program.cs
+++ Second_semestr/Homework_5/Map/Map/Program.cs
@@ -7,25 +7,29 @@ namespace ProgramNamespace
     public class Program
     {
         /// <summary>
-        /// applies a given function to each element of a list, returning a list of results in the same order
+        /// applies a given function to each element of a list
+        /// and returns new list of results in the same order
         /// </summary>
         /// <param name="list"></param>
         /// <param name="convert"></param>
         /// <returns></returns>
-        public static void Map(List<int> list, Func<int, int> convert)
+        public static List<int> Map(List<int> list, Func<int, int> convert)
         {
             var sizeOfList = list.Count();
+            var newList = new List<int>();
             for (int i = 0; i < sizeOfList; ++i)
             {
-                list[i] = convert(list[i]);
+                newList.Add(convert(list[i]));
             }
+
+            return newList;
         }
 
         static void Main(string[] args)
         {
             var list = new List<int> { 1, 2, 5, 10 };
-            Map(list, element => element * 2);
-            foreach (var element in list)
+            var newList = Map(list, element => element * 2);
+            foreach (var element in newList)
             {
                 Console.Write(element + " ");
             }
