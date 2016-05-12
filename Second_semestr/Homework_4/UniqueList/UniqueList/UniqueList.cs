0389fd2b2ece5b0bb0265668e4a0046ac67edc4e
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-12 13:03:23 +0300

    changed implementation of additions to the Unique List

--- Second_semestr/Homework_4/UniqueList/UniqueList/UniqueList.cs
+++ Second_semestr/Homework_4/UniqueList/UniqueList/UniqueList.cs
@@ -1,5 +1,4 @@
 ﻿using ListNamespace;
-using MyException;
 
 namespace UniqueListNamespace
 {
@@ -9,30 +8,14 @@ namespace UniqueListNamespace
         /// duplicate values are ignored
         /// </summary>
         /// <param name="value"></param>
-        public override void AddListElement(int value)
+        public override void Add(int value)
         {
-            if (this.head == null)
-            {
-                this.head = new ListElement(value); ;
-                return;
-            }
-
-            if (this.head.Value == value)
+            if (this.Contains(value))
             {
                 throw new DuplicateValuesException("the list contains the given value");
             }
 
-            ListElement i = this.head;
-            while (i.Next != null)
-            {
-                if (i.Value == value)
-                {
-                    throw new DuplicateValuesException("the list contains the given value");
-                }
-
-                i = i.Next;
-            }
-            i.Next = new ListElement(value);
+            base.Add(value);
         }
 
         /// <summary>
@@ -41,46 +24,12 @@ namespace UniqueListNamespace
         /// <param name="value"></param>
         public override void AddWithKeepingOrder(int value)
         {
-            if (this.head == null)
-            {
-                this.head = new ListElement(value);
-                return;
-            }
-
-            if (this.head.Value == value)
+            if (this.Contains(value))
             {
                 throw new DuplicateValuesException("the list contains the given value");
             }
 
-            //Add to head
-            if (this.head.Value > value)
-            {
-                ListElement temp = this.head;
-                this.head = new ListElement(value);
-                this.head.Next = temp;
-                return;
-            }
-
-            //Add to sort list
-            ListElement i = this.head;
-            while (i.Next != null)
-            {
-                if (i.Value == value)
-                {
-                    throw new DuplicateValuesException("the list contains the given value");
-                }
-
-                if (i.Next.Value >= value)
-                {
-                    break;
-                }
-
-                i = i.Next;
-            }
-
-            ListElement newElement = new ListElement(value);
-            newElement.Next = i.Next;
-            i.Next = newElement;
+            base.AddWithKeepingOrder(value);
         }
     }
 }
