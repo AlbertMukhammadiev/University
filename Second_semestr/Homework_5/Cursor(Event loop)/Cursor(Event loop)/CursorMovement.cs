b06a45c5ae6448cffbd2955cd4be2ab2caed112f
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-14 20:16:10 +0300

    movement of the cursor in the console  using an event handler

---
+++ Second_semestr/Homework_5/Cursor(Event loop)/Cursor(Event loop)/CursorMovement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CursorNamespace
+{
+    /// <summary>
+    /// class that implements the movement of cursor
+    /// </summary>
+    class CursorMovement
+    {
+        public void Leftward(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft == 0)
+            {
+                Console.CursorLeft = 79;
+                return;
+            }
+
+            --Console.CursorLeft;
+        }
+
+        public void Rightward(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft == 79)
+            {
+                Console.CursorLeft = 0;
+                return;
+            }
+
+            ++Console.CursorLeft;
+        }
+
+        public void Upward(object sender, EventArgs args)
+        {
+            if (Console.CursorTop == 0)
+            {
+                Console.CursorTop = 299;
+                return;
+            }
+
+            --Console.CursorTop;
+        }
+
+        public void Downward(object sender, EventArgs args)
+        {
+            if (Console.CursorTop == 299)
+            {
+                Console.CursorTop = 0;
+                return;
+            }
+
+            ++Console.CursorTop;
+        }
+    }
+}
---
+++ Second_semestr/Homework_5/Cursor(Event loop)/Cursor(Event loop)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursorNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var movement = new CursorMovement();
+
+            eventLoop.LeftHandler += movement.Leftward;
+            eventLoop.RightHandler += movement.Rightward;
+            eventLoop.UpHandler += movement.Upward;
+            eventLoop.DownHandler += movement.Downward;
+
+            var log = new List<string>();
+
+            eventLoop.LeftHandler += (sender, eventArgs) => log.Add("left");
+            eventLoop.RightHandler += (sender, eventArgs) => log.Add("right");
+            eventLoop.UpHandler += (sender, eventArgs) => log.Add("up");
+            eventLoop.DownHandler += (sender, eventArgs) => log.Add("down");
+            Console.Write("press Esc to exit, press P to print log, press right/left/up/down arrow to move cursor.");
+            eventLoop.Run(log);
+        }
+    }
+}
