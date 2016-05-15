c71f04df3c8211e458cf57ccba6838bb13192725
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-15 17:17:04 +0300

    List<T>, that implements the interface IEnumerable

---
+++ Second_semestr/Homework_7/List(T)/List(T)/IList.cs
@@ -0,0 +1,43 @@
+﻿namespace ListNamespace
+{
+    /// <summary>
+    /// interface of singly linked list
+    /// </summary>
+    interface IList<T>
+    {
+        /// <summary>
+        /// adds an object to the end of the List
+        /// </summary>
+        void Add(T value);
+
+        /// <summary>
+        /// displays all elements of List into the console
+        /// </summary>
+        void Print();
+
+        /// <summary>
+        /// removes the first occurrence of a specific object from the List
+        /// </summary>
+        /// <param name="value"></param>
+        void Remove(T value);
+
+        ///// <summary>
+        ///// adds a new element to the List with keeping order of elements
+        ///// </summary>
+        ///// <param name="value"></param>
+        //void AddWithKeepingOrder(T value);
+
+        /// <summary>
+        /// returns a value of the List item with the specified number
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        T GetValue(int i);
+
+        /// <summary>
+        /// Determines whether an element is in the List
+        /// </summary>
+        /// <param name="item"></param>
+        bool Contains(T value);
+    }
+}
---
+++ Second_semestr/Homework_7/List(T)/List(T)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
