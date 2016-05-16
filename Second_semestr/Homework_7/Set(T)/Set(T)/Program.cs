cc3b0162101c66e2d15ce9ec2cb938176d3c214f
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-16 20:31:44 +0300

    nothing

---
+++ Second_semestr/Homework_7/Set(T)/Set(T)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Second_semestr/Homework_7/Set(T)/Set(T)/Set.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetNamespace
+{
+    public class Set<T>
+    {
+        private class SetElement
+        {
+            T value;
+
+        }
+    }
+}
