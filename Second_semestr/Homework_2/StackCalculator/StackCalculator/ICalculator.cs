208b61ebacfbdde526c82ce7b572e98c980958a1
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-19 19:17:53 +0300

    Stack calculator without comments and unit tests

---
+++ Second_semestr/Homework_2/StackCalculator/StackCalculator/ICalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace CalculatorNamespace
+{
+    /// <summary>
+    /// interface of stack calculator
+    /// </summary>
+    interface ICalculator
+    {
+        void Add();
+
+        void Subtract();
+
+        void Multiply();
+
+        void Divide();
+
+        void Push(int value);
+
+        int Result();
+    }
+}
---
+++ Second_semestr/Homework_2/StackCalculator/StackCalculator/Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace StackNamespace
+{
+    /// <summary>
+    /// LIFO
+    /// </summary>
+    public class Stack : IStack
+    {
+        public void Push(int value)
+        {
+            StackElement newElement = new StackElement(value);
+            if (this.head == null)
+            {
+                this.head = newElement;
+                return;
+            }
+
+            newElement.Next = this.head;
+            this.head = newElement;
+        }
+
+        public int Top()
+        {
+            if (this.head == null)
+            {
+                throw new EmptyStackException("you are trying to access a non-existent object");
+            }
+
+            return this.head.Value;
+        }
+
+        public void Pop()
+        {
+            if (this.head == null)
+            {
+                throw new EmptyStackException("you are trying to access a non-existent object");
+            }
+
+            this.head = this.head.Next;
+        }
+
+        public bool IsEmpty() => this.head == null;
+
+        public void PrintStack()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            StackElement i = this.head;
+            while (i != null)
+            {
+                Console.WriteLine(i.Value + " ");
+                i = i.Next;
+            }
+        }
+
+        private class StackElement
+        {
+            /// <summary>
+            /// class constructor
+            /// </summary>
+            /// <param name="value"></param>
+            public StackElement(int value)
+            {
+                this.Value = value;
+            }
+
+            public int Value { get; set; }
+            public StackElement Next { get; set; }
+        }
+
+        private StackElement head;
+    }
+}
