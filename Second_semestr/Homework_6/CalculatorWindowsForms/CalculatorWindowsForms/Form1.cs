3b951aa6438fdd64acc94492dbfb094c369f9323
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-21 20:57:17 +0300

    Calculator (Windows Forms)

---
+++ Second_semestr/Homework_6/CalculatorWindowsForms/CalculatorWindowsForms/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using StackCalculatorNamespace;
+
+namespace CalculatorWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonNumbersClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            buffer += button.Text;
+            display.Text += button.Text;
+        }
+
+        private void OnOperationClick(object sender, EventArgs e)
+        {
+            if (buffer == "")
+            {
+                return;
+            }
+
+            try
+            {
+                calculator.Push(Convert.ToDouble(buffer));
+            }
+            catch (FormatException)
+            {
+                this.OnCClick(1, e);
+                display.Text = "Ошибка:";
+                display2.Text = "Вы ввели число в некорректном формате";
+                return;
+            }
+            
+            if (sign != '\0')
+            {
+                try
+                {
+                    calculator.Operation(sign);
+                }
+                catch (MyDivideByZeroException)
+                {
+                    this.OnCClick(sender, e);
+                    display2.Text = "Ошибка: Делить на ноль нельзя";
+                    return;
+                }
+
+                display2.Text = Convert.ToString(calculator.Result());
+                display.Text = display2.Text;
+            }
+
+            buffer = "";
+            var button = (Button)sender;
+            sign = button.Text[0];
+            display.Text += button.Text;
+        }
+
+        private void OnEqualClick(object sender, EventArgs e)
+        {
+            if (calculator.IsEmpty())
+            {
+                return;
+            }
+
+            if (buffer != "")
+            {
+                try
+                {
+                    calculator.Push(Convert.ToDouble(buffer));
+                    calculator.Operation(sign);
+                }
+                catch (FormatException)
+                {
+                    this.OnCClick(sender, e);
+                    display2.Text = "Ошибка: Вы ввели число в некорректном формате";
+                    return;
+                }
+                catch (MyDivideByZeroException)
+                {
+                    this.OnCClick(sender, e);
+                    display2.Text = "Ошибка: Делить на ноль нельзя";
+                    return;
+                }
+            }
+
+            display.Text = Convert.ToString(calculator.Result());
+            display2.Text = "";
+            buffer = display.Text;
+            sign = '\0';
+        }
+
+        private void OnCClick(object sender, EventArgs e)
+        {
+            buffer = "";
+            display.Text = "";
+            display2.Text = "";
+            sign = '\0';
+            calculator.Clear();
+        }
+
+        private string buffer = "";
+        private StackCalculator calculator = new StackCalculator();
+        private char sign = '\0';
+    }
+}
---
+++ Second_semestr/Homework_6/CalculatorWindowsForms/CalculatorWindowsForms/StackCalculator.cs
@@ -0,0 +1,103 @@
+﻿using StackNamespace;
+
+namespace StackCalculatorNamespace
+{
+    /// <summary>
+    /// class that implements a stack-based calculator
+    /// </summary>
+    public class StackCalculator : IStackCalculator
+    {
+        public double Result() => stack.Top();
+
+        public bool IsEmpty() => stack.IsEmpty();
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+        public void Push(double value)
+        {
+            stack.Push(value);
+        }
+
+        public void Operation(char sign)
+        {
+            switch (sign)
+            {
+                case '+':
+                    {
+                        this.SumUp();
+                        break;
+                    }
+                    
+                case '-':
+                    {
+                        this.Subtract();
+                        break;
+                    }
+                    
+                case '*':
+                    {
+                        this.Multiply();
+                        break;
+                    }
+
+                case '/':
+                    {
+                        this.Divide();
+                        break;
+                    }
+                    
+                default:
+                    {
+                        //Console.WriteLine("Default case");
+                        break;
+                    }
+            }
+        }
+
+        private void SumUp()
+        {
+            var first = stack.Top();
+            stack.Pop();
+            var second = stack.Top();
+            stack.Pop();
+            stack.Push(first + second);
+        }
+
+        private void Subtract()
+        {
+            var first = stack.Top();
+            stack.Pop();
+            var second = stack.Top();
+            stack.Pop();
+            stack.Push(second - first);
+        }
+
+        private void Multiply()
+        {
+            var first = stack.Top();
+            stack.Pop();
+            var second = stack.Top();
+            stack.Pop();
+            stack.Push(first * second);
+        }
+
+        private void Divide()
+        {
+            var first = stack.Top();
+            if (first == 0)
+            {
+                throw new MyDivideByZeroException("you can not divide by zero");
+            }
+
+            stack.Pop();
+            var second = stack.Top();
+            stack.Pop();
+            stack.Push(second / first);
+        }
+
+        private Stack<double> stack = new Stack<double>();
+    }
+}
