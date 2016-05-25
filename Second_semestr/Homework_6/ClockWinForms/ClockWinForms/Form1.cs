ed9279eceffae8d1fe2a182a0ef5c2162658d8f8
AlbertMukhammadiev <beehead02@gmail.com>
2016-05-25 13:22:55 +0300

    added arrows

--- Second_semestr/Homework_6/ClockWinForms/ClockWinForms/Form1.cs
+++ Second_semestr/Homework_6/ClockWinForms/ClockWinForms/Form1.cs
@@ -17,19 +17,38 @@ namespace ClockWinForms
 
 
             Timer time = new Timer();
-            time.Interval = 500;
+            time.Interval = 100;
             time.Tick += Arrow;
             time.Enabled = true;
+
+            Timer time2 = new Timer();
+            time2.Interval = 10000;
+            time2.Tick += Arrow2;
+            time2.Enabled = true;
+
+            Timer time3 = new Timer();
+            time3.Interval = 800;
+            time3.Tick += Arrow3;
+            time3.Enabled = true;
         }
 
-        Pen MyPen = new Pen(Color.Red, 5);
-        float x2 = 175;
-        float y2 = 50;
+        Pen MyPen = new Pen(Color.DarkSeaGreen, 5);
+        float x2 = 200;
+        float y2 = 20;
         int fi = -90;
-        int r = 125;
+        int r = 175;
 
-        
+        Pen MyPen2 = new Pen(Color.Pink, 10);
+        float x22 = 200;
+        float y22 = 100;
+        int fi2 = -90;
+        int r2 = 100;
 
+        Pen MyPen3 = new Pen(Color.DarkOrchid, 2);
+        float x23 = 200;
+        float y23 = 100;
+        int fi3 = -90;
+        int r3 = 150;
 
         ////////public SomeForm()
         ////////{
@@ -76,22 +95,38 @@ namespace ClockWinForms
             ////e.Graphics.DrawImageUnscaled(drawing, 0, 0);
             ////g.Dispose();
 
-            ////time.Interval = 500;
-            ////time.Tick += Arrow;
-            ////time.Enabled = true;
-
 
             e.Graphics.DrawEllipse(MyPen, 10, 10, 380, 380);
             e.Graphics.DrawLine(MyPen, 200, 200, x2, y2);
-
-            
-
-
-
+            e.Graphics.DrawLine(MyPen2, 200, 200, x22, y22);
+            e.Graphics.DrawLine(MyPen3, 200, 200, x23, y23);
         }
 
 
+        private void Arrow2(object sender, EventArgs e)
+        {
 
+            ClockFace.Invalidate();
+            fi2 += 30;
+            float cosFi = (float)Math.Cos(((Math.PI * fi2) / 180));
+            float sinFi = (float)Math.Sin(((Math.PI * fi2) / 180));
+
+            x22 = 200 + r2 * cosFi;
+            y22 = 200 + r2 * sinFi;
+        }
+
+        private void Arrow3(object sender, EventArgs e)
+        {
+
+            ClockFace.Invalidate();
+            fi3 += 60;
+            float cosFi = (float)Math.Cos(((Math.PI * fi3) / 180));
+            float sinFi = (float)Math.Sin(((Math.PI * fi3) / 180));
+
+            x23 = 200 + r3 * cosFi;
+            y23 = 200 + r3 * sinFi;
+
+        }
 
         private void Arrow(object sender, EventArgs e)
         {
@@ -104,17 +139,6 @@ namespace ClockWinForms
             x2 = 200 + r * cosFi;
             y2 = 200 + r * sinFi;
 
-        }
-
-        private void Movement(object sender, EventArgs e)
-        {
-
-            fi += 6;
-            float cosFi = (float)Math.Cos(((Math.PI * fi) / 180));
-            float sinFi = (float)Math.Sin(((Math.PI * fi) / 180));
-
-            x2 = 200 + r * cosFi;
-            y2 = 200 + r * sinFi;
         }
     }
 }
