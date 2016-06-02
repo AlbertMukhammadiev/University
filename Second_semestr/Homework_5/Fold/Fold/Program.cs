a7ec744c71f0010442fb243c15b7a220d5023074
AlbertMukhammadiev <beehead02@gmail.com>
2016-06-03 00:14:33 +0300

    Fold (using λ-function)

---
+++ Second_semestr/Homework_5/Fold/Fold/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgramNamespace
+{
+    public class Program
+    {
+        /// <summary>
+        /// converts data structures to a single atomic value using the given function
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="convert"></param>
+        /// <returns></returns>
+        public static int Fold(List<int> list, int acc, Func<int, int, int> convert)
+        {
+            int result = acc;
+            foreach (var element in list)
+            {
+                result = convert(result, element);
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fold(new List<int>() { 1, 0, 3 }, 1, (acc, elem) => acc + elem));
+        }
+    }
+}
