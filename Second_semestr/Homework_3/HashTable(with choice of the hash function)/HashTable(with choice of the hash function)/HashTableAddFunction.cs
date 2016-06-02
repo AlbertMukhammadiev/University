6993f9995595f03f87d034fc0f637c3564daf5e0
AlbertMukhammadiev <beehead02@gmail.com>
2016-06-02 22:50:45 +0300

    different hash functions

---
+++ Second_semestr/Homework_3/HashTable(with choice of the hash function)/HashTable(with choice of the hash function)/HashTableAddFunction.cs
@@ -0,0 +1,31 @@
+﻿namespace HashTableNamespace
+{
+    /// <summary>
+    /// hash table, that hashes by the sum of all letters of the word
+    /// </summary>
+    public class HashTableAddFunction : HashTable
+    {
+        public HashTableAddFunction(int size) : base(size)
+        {
+
+        }
+
+        /// <summary>
+        /// applies some function to the word
+        /// and returns the key that corresponds to the given parameter
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public override int HashFunction(string word)
+        {
+            int result = 0;
+            int length = word.Length;
+            for (int i = 0; i < length; ++i)
+            {
+                result += word[i];
+            }
+
+            return result % this.Size;
+        }
+    }
+}
