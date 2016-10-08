24b44668f92ccb9c491e7c199d495fa9a661edb9
AlbertMukhammadiev <beehead02@gmail.com>
2016-10-09 00:12:50 +0300

    ..

--- Third_semestr/Homework_1/IteratorForBinaryTree/IteratorForBinaryTree/Program.cs
+++ Third_semestr/Homework_1/IteratorForBinaryTree/IteratorForBinaryTree/Program.cs
@@ -11,23 +11,6 @@ namespace ProgramNamespace
     {
         static void Main(string[] args)
         {
-            BinaryTree<string> tree = new BinaryTree<string>();
-
-            tree.Insert(50, "и");
-            tree.Insert(60, "н");
-            tree.Insert(40, "л");
-            tree.Insert(55, "в");
-            tree.Insert(70, "о");
-            tree.Insert(30, "р");
-            tree.Insert(53, "т");
-            tree.Insert(58, "и");
-            tree.Insert(35, "й");
-            tree.Insert(33, "и");
-            tree.Insert(37, " ");
-            tree.Insert(80, "в");
-            tree.Insert(20, "ю");  
-
-            tree.PrintTree(0);
 
         }
     }
