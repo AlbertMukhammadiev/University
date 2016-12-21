ef8f143e1f3c28d831006f7dcfaee62565ae8b1f
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-21 15:14:36 +0300

    class Computer

---
+++ Third_semestr/Homework_2/ModelOfLAN/ModelOfLAN/Computer.cs
@@ -0,0 +1,74 @@
+﻿namespace ModelOfLAN
+{
+    public enum OS { Windows, Linux, OS_X};
+
+    /// <summary>
+    /// computer of local network
+    /// </summary>
+    public class Computer
+    {
+        /// <summary>
+        /// class constructor
+        /// </summary>
+        /// <param name="abbr">type of operating system</param>
+        public Computer(char abbr)
+        {
+            switch (abbr)
+            {
+                case 'w':
+                    {
+                        this.operatingSystem = OS.Windows;
+                        this.helthPoints = 60;
+                        break;
+                    }
+                case 'l':
+                    {
+                        this.operatingSystem = OS.Linux;
+                        this.helthPoints = 70;
+                        break;
+                    }
+                case 'o':
+                    {
+                        this.operatingSystem = OS.OS_X;
+                        this.helthPoints = 50;
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// type of operating system
+        /// </summary>
+        public OS operatingSystem { get; set; }
+
+        /// <summary>
+        /// ID of computer in local network
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// checks the health of computer
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInfected() => this.isInfected;
+
+        /// <summary>
+        /// simulates a viral attack on the computer
+        /// </summary>
+        /// <param name="damagePoints">the power of viral attacks</param>
+        public void Infect(int damagePoints)
+        {
+            if (damagePoints >= helthPoints)
+            {
+                isInfected = true;
+            }
+        }
+
+        private int helthPoints;
+        private bool isInfected;
+    }
+}
