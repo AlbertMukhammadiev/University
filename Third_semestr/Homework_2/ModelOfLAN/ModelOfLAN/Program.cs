d919e0f8403c4bcb50626a7981c733021cf5c585
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-21 15:18:23 +0300

    point of entry

---
+++ Third_semestr/Homework_2/ModelOfLAN/ModelOfLAN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelOfLAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new System.IO.StreamReader(@"C:\Users\Альберт\Documents\GitHub\University\Third_semestr\Homework_2\ModelOfLAN\test.txt");
+            var list = new List<int> { 11, 3 };
+            var lan = new LocalNetwork(file, list);
+            file.Close();
+
+            lan.ShowSystem();
+            Console.WriteLine();
+
+            while (!lan.IsUnhealthy())
+            {
+                lan.Step();
+                lan.ShowSystem();
+                Console.WriteLine();
+                Console.WriteLine("System is unhealthy " + lan.IsUnhealthy());
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
