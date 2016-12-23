3ea0a2fae1a4cf720ab1de46e594f236751d421e
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-23 10:56:42 +0300

    for test

--- Third_semestr/Homework_3/Robots/Robots.Tests/RobotsTest.cs
+++ Third_semestr/Homework_3/Robots/Robots.Tests/RobotsTest.cs
@@ -1,14 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robots;
 
 namespace RobotsTests
 {
     [TestClass]
     public class UnitTest1
     {
+        System.IO.StreamReader file;
+        List<int> robots;
+        Graph twoToneGraph;
+        Graph monochromeGraph;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            robots = new List<int> { 11, 3 };
+            file = new System.IO.StreamReader(@"C:\Users\Альберт\Documents\GitHub\University\Third_semestr\Homework_3\Robots\MonochromeGraph.txt");
+            monochromeGraph = new Graph(file, robots);
+            file.Close();
+
+            file = new System.IO.StreamReader(@"C:\Users\Альберт\Documents\GitHub\University\Third_semestr\Homework_3\Robots\TwoToneGraph.txt");
+            twoToneGraph = new Graph(file, robots);
+            file.Close();
+        }
+
+
         [TestMethod]
         public void RobotsTest()
         {
+
         }
     }
 }
