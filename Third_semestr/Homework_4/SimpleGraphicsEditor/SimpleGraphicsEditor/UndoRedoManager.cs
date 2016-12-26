973fff6c154edbc14948d0579e2f9dee49d1579d
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-26 16:14:15 +0300

    added manager for undo/redo

---
+++ Third_semestr/Homework_4/SimpleGraphicsEditor/SimpleGraphicsEditor/UndoRedoManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraphicsEditor
+{
+    public class UndoRedoManager
+    {
+        public void Undo()
+        {
+            if (UndoStack.Count > 0)
+            {
+                var command = UndoStack.Pop();
+                command.UnExecute();
+                RedoStack.Push(command);
+            }
+        }
+
+        public void Redo()
+        {
+            if (RedoStack.Count > 0)
+            {
+                var command = RedoStack.Pop();
+                command.Execute();
+                UndoStack.Push(command);
+            }
+        }
+
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+            UndoStack.Push(command);
+            //RedoStack.Clear();
+        }
+
+        private Stack<ICommand> UndoStack = new Stack<ICommand>();
+        private Stack<ICommand> RedoStack = new Stack<ICommand>();
+    }
+}
+
