41a87a6f08d4bdff9b5d6a609c98b882eb2dd173
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-28 23:39:41 +0300

    models were grouped in one folder

---
+++ Third_semestr/Homework_4/SimpleGraphicsEditor/SimpleGraphicsEditor/Models/Line.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace SimpleGraphicsEditor.Models
+{
+    /// <summary>
+    /// this clas represents Line
+    /// </summary>
+    public class Line : Shape
+    {
+        /// <summary>
+        /// class constructor
+        /// </summary>
+        /// <param name="visible"></param>
+        /// <param name="parameter"></param>
+        public Line(bool visible, Parameters parameter) : base(visible, parameter)
+        {
+            this.Parameter = parameter;
+        }
+
+        public override bool IsSeized(Point p)
+        {
+            //throw new NotImplementedException();
+            var distanceFirst = Math.Sqrt(Math.Pow(p.X - this.Parameter.Start.X, 2) + Math.Pow(p.Y - this.Parameter.Start.Y, 2));
+            var distanceSecond = Math.Sqrt(Math.Pow(p.X - this.Parameter.Move.X, 2) + Math.Pow(p.Y - this.Parameter.Move.Y, 2));
+            var distance = distanceFirst <= distanceSecond ? distanceFirst : distanceSecond;
+            if (distanceFirst <= distanceSecond)
+            {
+                var point = this.Parameter.Start;
+                this.Parameter.Start = this.Parameter.Move;
+                this.Parameter.Move = point;
+            }
+
+            return distance < 10;
+        }
+
+        public override void Draw(System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(Parameter.Pen, Parameter.Start, Parameter.Move);
+        }
+    }
+}
---
+++ Third_semestr/Homework_4/SimpleGraphicsEditor/SimpleGraphicsEditor/Models/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleGraphicsEditor.Models
+{
+    /// <summary>
+    /// this class represents the shape
+    /// </summary>
+    public abstract class Shape
+    {
+        /// <summary>
+        /// class constuctor
+        /// </summary>
+        /// <param name="visible"></param>
+        /// <param name="parameter"></param>
+        public Shape(bool visible, Parameters parameter)
+        {
+            this.Parameter = parameter;
+        }
+
+        /// <summary>
+        /// checks if the shape is caught
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public abstract bool IsSeized(Point p);
+
+        /// <summary>
+        /// the shape paints itself
+        /// </summary>
+        /// <param name="e"></param>
+        public abstract void Draw(PaintEventArgs e);
+
+        /// <summary>
+        /// some parameters of shape
+        /// </summary>
+        public Parameters Parameter { get; set; }
+    }
+}
