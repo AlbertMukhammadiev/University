daf00f0267f7ab0048f3e70831522b26101db691
AlbertMukhammadiev <beehead02@gmail.com>
2017-02-28 20:23:14 +0300

    calculating Fibonacci numbers

---
+++ Fourth_semestr/Homework_1/FibonacciNumbers/FibonacciNumbers/Program.fs
@@ -0,0 +1,23 @@
+﻿let rec calcFibonacci i n acc1 acc2 =
+    if i = n then
+        acc1 + acc2
+    else
+        calcFibonacci
+            (i + 1)
+            (n)
+            (acc2)
+            (acc1 + acc2)
+
+let fibonacci n =
+    if n >= 0  then
+        calcFibonacci 0 n -1 1
+    else
+        if ((-1 * n + 1) % 2 = 0) then
+            calcFibonacci 0 -n -1 1
+        else
+            -1 * calcFibonacci 0 -n -1 1
+        
+[<EntryPoint>]
+let main argv = 
+    printfn "%d th fibonacci number is  %d" 6 (fibonacci 6)
+    0 // возвращение целочисленного кода выхода
