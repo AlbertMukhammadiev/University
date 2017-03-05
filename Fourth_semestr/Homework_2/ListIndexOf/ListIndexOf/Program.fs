e96c9ac6eb9f3bbe14b49bcb3391f3d825d714e6
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-05 22:44:05 +0300

    function List.IndexOf

---
+++ Fourth_semestr/Homework_2/ListIndexOf/ListIndexOf/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec indexOf elem list i =
+    match list with
+    | [] -> None
+    | head :: tail -> if (head <> elem) then indexOf (elem) (tail) (i + 1)
+                      else Some i
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
