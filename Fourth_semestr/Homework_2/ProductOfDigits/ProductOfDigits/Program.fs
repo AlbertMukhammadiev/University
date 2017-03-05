9e574ca4cc33889df97172b5de1cb4f4f6dd130c
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-05 19:49:30 +0300

    two variants of the function 'product' which considers the product of the digits

---
+++ Fourth_semestr/Homework_2/ProductOfDigits/ProductOfDigits/Program.fs
@@ -0,0 +1,26 @@
+﻿let rec product acc num =
+    if num / 10 = 0 then
+        acc * num
+    else
+        if num % 10 = 0 then
+            0
+        else
+            product
+                (acc * (num % 10))
+                (num / 10)
+
+
+let rec listOfDigits num acc =
+    if num / 10 = 0 then
+        num :: acc
+    else
+        listOfDigits
+            (num / 10)
+            ((num % 10) :: acc)
+
+let product1 num = List.fold(fun acc elem -> acc * elem) 1 (listOfDigits num []) 
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
