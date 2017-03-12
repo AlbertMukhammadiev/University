9c9fd64c7e21aa852ac566500a3bf006dea38c19
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-12 17:07:08 +0300

    function that checks for equivalent elements in list

---
+++ Fourth_semestr/Homework_3/ListIsUnique/ListIsUnique/Program.fs
@@ -0,0 +1,12 @@
+﻿let rec isUniqueRec list acc =
+    match list with
+    | [] -> true
+    | head :: tail -> if List.contains head acc then false
+                      else isUniqueRec (tail) (head :: acc)
+                    
+let isUnique list = isUniqueRec list []
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
