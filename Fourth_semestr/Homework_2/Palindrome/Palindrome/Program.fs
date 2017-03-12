583c1864a98a26bb088bfae4589e387546254fd5
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-12 15:35:47 +0300

    one more version of the function isPalindrome

--- Fourth_semestr/Homework_2/Palindrome/Palindrome/Program.fs
+++ Fourth_semestr/Homework_2/Palindrome/Palindrome/Program.fs
@@ -23,9 +23,12 @@ let isPalindrome1 = check true << listOfPairs
 
 let isPalindrome2 str = List.forall2 (fun elem1 elem2 -> elem1 = elem2) (List.rev(strList 0 [] str)) (strList 0 [] str)
 
+let isPalindrome3 str = List.ofSeq str = List.rev (List.ofSeq str)
+
 [<EntryPoint>]
 let main argv = 
 //    printfn "%A" argv
     printfn "%b" (isPalindrome1 "AbvgdgvbA")
     printfn "%b" (isPalindrome2 "AbvgdvbA")
+    printfn "%b" (isPalindrome3 "AbvgdvbA")
     0 // возвращение целочисленного кода выхода
