0e0e0e961f15102f468a6565e8683c3613d623c4
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-12 20:42:05 +0300

    function finds index of max sum of neighbors using list of trio

---
+++ Fourth_semestr/Homework_3/List_IndexOf_maxSumOfNeigh/List_IndexOf_maxSumOfNeigh/Program.fs
@@ -0,0 +1,18 @@
+﻿let shift list =
+    match list with
+    | [] -> []
+    | [x] -> list
+    | head :: tail -> tail @ (head :: [])
+
+let indexOfmaxSum list =
+    let triple = List.sortDescending << List.map (fun (x, i, elem) -> (x + elem, i, elem)) <| List.zip3 (shift list) [1 .. list.Length] list
+    match triple with
+    | [] -> None
+    | head :: tail -> match head with
+                      | (x, y, z) -> Some y
+
+[<EntryPoint>]
+let main argv = 
+//    printfn "%A" argv
+    printfn "%A" (indexOfmaxSum [1;5;6;2])
+    0 // возвращение целочисленного кода выхода
