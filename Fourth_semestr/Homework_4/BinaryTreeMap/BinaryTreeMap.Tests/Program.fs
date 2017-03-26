68c3ce0e094e11bbbdf4c568a296359275c5dedf
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-26 22:39:40 +0300

    project for tests

---
+++ Fourth_semestr/Homework_4/BinaryTreeMap/BinaryTreeMap.Tests/Program.fs
@@ -0,0 +1,11 @@
+﻿open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``that is perfect should get a score of 300.``() =
+	scoreBowls [for i in 1..18 -> 10] |> should equal 300
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
