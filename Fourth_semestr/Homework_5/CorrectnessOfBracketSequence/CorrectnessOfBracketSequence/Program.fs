352ab447ca1de42bb1777404281419d8b21c1a35
AlbertMukhammadiev <beehead02@gmail.com>
2017-03-26 22:42:53 +0300

    correctness of bracket sequence

---
+++ Fourth_semestr/Homework_5/CorrectnessOfBracketSequence/CorrectnessOfBracketSequence/Program.fs
@@ -0,0 +1,22 @@
+﻿let check (str : string) =
+    let rec checkTR str stack=
+        match str with
+        | [] -> stack
+        | head :: tail -> match head with
+                          | "(" | "[" | "{" -> checkTR tail (head :: stack)
+                          | ")" | "]" | "}" -> match stack with
+                                               | [] -> [head]
+                                               | top :: rest -> let expr = top + head
+                                                                if expr <> "{}" || expr <> "()" || expr <> "[]" then
+                                                                    stack
+                                                                else
+                                                                    checkTR tail rest
+                          | x -> checkTR tail stack
+    let s = List.map string <| List.ofSeq str
+    (checkTR s []).IsEmpty
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    check "(){}[{(){}}]"
+    0 // возвращение целочисленного кода выхода
