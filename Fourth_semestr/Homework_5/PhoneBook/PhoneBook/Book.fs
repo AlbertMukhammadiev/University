5680c222c7fa419157f1360fe08a62de851944af
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-14 09:40:09 +0300

    phone book

---
+++ Fourth_semestr/Homework_5/PhoneBook/PhoneBook/Book.fs
@@ -0,0 +1,47 @@
+﻿module Book
+
+open System.IO
+
+type Record = { Name : string; PhoneNumber : string }
+
+let path = "C:\Users\Альберт\Documents\GitHub\University\Fourth_semestr\Homework_5\PhoneBook\PhoneBook\Book.txt"
+
+let mutable records = Seq.empty
+
+let mutable buffer = List.empty
+
+let read () =
+    records <-
+    seq { use reader = new StreamReader(File.OpenRead path)
+          while not reader.EndOfStream do
+            let record = reader.ReadLine().Split()
+            yield (record.[0], record.[1]) }
+
+let read2 fn =
+    seq { use input = File.OpenText fn in
+          while not input.EndOfStream do
+            yield input.ReadLine() }   
+            
+let add (x : string, y : string) =
+    buffer <- (x, y) :: buffer
+
+let findByName name =
+    let record = Seq.tryFind (fun (x, y) -> name = x) records
+    match record with
+    | Some (name, num) -> num
+    | None -> "There is no person with such name"
+
+let findByNumber number =
+    let record = Seq.tryFind (fun (x, y) -> number = y) records
+    match record with
+    | Some (name, num) -> name
+    | None -> "There is no person with such a telephone number"
+
+let print () =
+    let data = Seq.readonly <| read2 path
+    Seq.iter (fun elem -> printfn "%A" elem) data
+
+let save () =
+    let tosave = Seq.fold (fun acc (x, y) -> (x + " " + y) :: acc  ) [] <| Seq.distinct buffer
+    buffer <- List.empty
+    File.AppendAllLines (path, tosave)
---
+++ Fourth_semestr/Homework_5/PhoneBook/PhoneBook/Tests.fs
@@ -0,0 +1,58 @@
+﻿module Tests
+
+open Book
+open NUnit.Framework
+open FsUnit
+
+//System.IO.Directory.SetCurrentDirectory "C:\Users\Альберт\Documents\GitHub\University\Fourth_semestr\Homework_5\PhoneBook\PhoneBook"
+
+[<Test>]
+let ``test of searching number of the Police`` () =
+    read ()
+    findByName "Police" |> should equal "112102"
+     
+[<Test>]
+let ``test of searching the name by number 112144`` () =
+    read ()
+    findByNumber "112144" |> should equal "GasEmergency"
+
+[<Test>]
+let ``test of add to data base(checking number of records)`` () =
+    read ()
+    let prev = Seq.length records
+    add ("BabushkaDedushka1", "25202")
+    add ("BabushkaDedushka2", "23124")
+    add ("MamaPapa", "31496")
+    save ()
+    read ()
+    let current = Seq.length records
+    current - prev |> should equal 3
+
+[<Test>]
+let ``test of add to data base(searching of the added record)`` () =
+    read ()
+    add ("DedMoroz", "88005553555")
+    save ()
+    read ()
+    Seq.tryFind (fun (name, num) -> (name, num) = ("DedMoroz", "88005553555")) |> should not' (equal None)
+
+[<Test>]
+let ``test of save (checking number of records)`` () =
+    read ()
+    let length = Seq.length records
+    add ("Matroskin", "82466")
+    add ("Serega", "-") 
+    save ()
+    save ()
+    save ()
+    read ()
+    Seq.length records |> should equal (2 + length)
+
+[<Test>]
+let ``adding two equal records`` () =
+    read ()
+    let length = Seq.length records
+    add ("home", "31496")
+    add ("home", "31496")
+    save ()
+    Seq.length records |> should equal (1 + length)
