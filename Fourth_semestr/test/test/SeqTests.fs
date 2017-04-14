469911ee425cf53ee13fe54850df1384ebfc337f
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-14 13:16:57 +0300

    added two tests

--- Fourth_semestr/test/test/SeqTests.fs
+++ Fourth_semestr/test/test/SeqTests.fs
@@ -16,9 +16,16 @@ let ``length of sequence of 1000 elements should equal 1000`` () =
     Seq.length seq |> should equal 1000
 
 [<Test>]
-let ``test of llsad`` () =
+let ``sum of elements`` () =
     let seq = Seq.take 15 sequence
-    Seq.
+    Seq.sum seq |> should equal 55
+
+[<Test>]
+let ``prev must be <= that next`` () =
+    let seq = Seq.take 1000 sequence
+    let seq2 = Seq.init 20 (fun x -> x)
+    Seq.forall (fun elem -> elem = (Seq.length <| Seq.filter (fun x -> x = elem) seq)) seq2 |> should equal true
+
 
 //[<Test>]
 //Check.Quick getSeq
