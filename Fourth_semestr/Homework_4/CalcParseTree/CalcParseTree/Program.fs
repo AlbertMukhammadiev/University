c70132853ed192bc1b6bf3890f11d5a17e10c4b1
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-16 22:30:21 +0300

    function that calculates parse tree

---
+++ Fourth_semestr/Homework_4/CalcParseTree/CalcParseTree/Program.fs
@@ -0,0 +1,16 @@
+﻿module ParseTree
+
+type Tree =
+    | Node of char * Tree * Tree
+    | Tip of int
+
+let rec calculate tree = 
+    match tree with
+    | Tip x -> x
+    | Node (oper, left, right) ->
+        match oper with
+        | '+' -> calculate left + calculate right
+        | '-' -> calculate left - calculate right
+        | '*' -> calculate left * calculate right
+        | '/' -> calculate left / calculate right
+        | _ -> failwith "incorrect operator"
