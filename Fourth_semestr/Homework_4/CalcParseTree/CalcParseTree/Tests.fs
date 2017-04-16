aec2547eee68ee699c8498d422e7337f595920c8
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-16 22:30:37 +0300

    tests

---
+++ Fourth_semestr/Homework_4/CalcParseTree/CalcParseTree/Tests.fs
@@ -0,0 +1,39 @@
+﻿module Tests
+
+open ParseTree
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Calculating of ((7 + 3) * (5 - 2))`` () =
+    let tree =
+        Node (
+            '*',
+            Node ('+', Tip 7, Tip 3),
+            Node ('-', Tip 5, Tip 2)
+            )
+    calculate tree |> should equal 30
+
+[<Test>]
+let ``provoking of throw of exception`` () =
+    let tree =
+        Node (
+            'w',
+            Node ('+', Tip 7, Tip 3),
+            Node ('-', Tip 5, Tip 2)
+            )
+    (fun () -> calculate tree |> ignore) |> should (throwWithMessage "incorrect operator") typeof<System.Exception> 
+
+[<Test>]
+let ``Calculating of (5 * (10 - (30 + 2)))`` () =
+    let tree =
+        Node (
+            '*',
+            Tip 5,
+            Node (
+                '-',
+                Tip 10,
+                Node ('+', Tip 30, Tip 2)
+                )
+            )
+    calculate tree |> should equal -110
