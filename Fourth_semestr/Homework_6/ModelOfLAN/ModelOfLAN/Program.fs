0c1176f1165022ed3f016ae764958dca385c87ad
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-19 23:12:18 +0300

    nothing

--- Fourth_semestr/Homework_6/ModelOfLAN/ModelOfLAN/Program.fs
+++ Fourth_semestr/Homework_6/ModelOfLAN/ModelOfLAN/Program.fs
@@ -1,6 +1,11 @@
-﻿
+﻿module Program
+
+open ModelOfLAN
+
+let model = new LocalNetwork("C:\Users\Альберт\Documents\GitHub\University\Fourth_semestr\Homework_6\ModelOfLAN\ModelOfLAN\matrix.txt", [2; 13])
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    model.ShowAdjList()
+    model.ShowSystem ()
     0 // возвращение целочисленного кода выхода
--- Fourth_semestr/Homework_6/ModelOfLAN/ModelOfLAN/testclass.fs
+++ Fourth_semestr/Homework_6/ModelOfLAN/ModelOfLAN/testclass.fs
@@ -6,6 +6,8 @@ type Vector(x:float, y:float) =
     member val XX = x with get, set
     member val YY = y with get, set 
 
-let f v =
-    let vector = Vector(1.0, 1.0)
-    vector.X <- 2.0
+//let f v =
+//    let vector = Vector(1.0, 1.0)
+//    vector.X <- 2.0
+
+
