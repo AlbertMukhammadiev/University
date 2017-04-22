620728473a78756e687a97c1f17ad97766bc5303
AlbertMukhammadiev <beehead02@gmail.com>
2017-04-22 14:24:35 +0300

    tests

---
+++ Fourth_semestr/Homework_6/ModelOfLAN/ModelOfLAN/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+
+open ModelOfLAN
+open NUnit.Framework
+open FsUnit
+
+type ModelOfLANTest() =
+    let LAN = new LocalNetwork("C:\Users\Альберт\Documents\GitHub\University\Fourth_semestr\Homework_6\ModelOfLAN\ModelOfLAN\matrix.txt", [3; 11])
+    
+    [<Test>]
+    let ``test of GetHealthyComputers`` () =
+        let ls = [0 .. 14]
+        let healthy = LAN.GetHealthyComputers()
+        Seq.sort healthy |> should equal ls
