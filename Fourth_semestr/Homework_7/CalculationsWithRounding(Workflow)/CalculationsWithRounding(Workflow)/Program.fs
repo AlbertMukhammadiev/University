ea8f7ecc2e6cff9a1e5984fbc15ad62a7b26a8b8
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-03 17:42:33 +0300

    start

---
+++ Fourth_semestr/Homework_7/CalculationsWithRounding(Workflow)/CalculationsWithRounding(Workflow)/Program.fs
@@ -0,0 +1,21 @@
+﻿module Program
+
+type MaybeBuid () =
+    member this.Bind(x, f) =
+        match x with
+        | None -> None
+        | Some a -> f a
+    member this.Return(x) = Some x
+
+let maybe = new MaybeBuid()
+
+//    rounding 3 {
+//        let! a = 2.0 / 12.0
+//        let! b = 3.5
+//        return a / b
+//    }
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // возвращение целочисленного кода выхода
