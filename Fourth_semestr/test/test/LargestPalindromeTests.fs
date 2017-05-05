0234b0c0f32c860a488d49e3d1672c714c23c827
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-05 12:43:26 +0300

    task 2    05.05.2017

---
+++ Fourth_semestr/test/test/LargestPalindromeTests.fs
@@ -0,0 +1,10 @@
+﻿module LargestPalindromeTests
+
+open LargestPalindrome
+open NUnit.Framework
+open FsUnit
+open FsCheck
+
+[<Test>]
+let ``test of largestPalindrome`` () =
+    () |> largestPalindrome |> should equal 906609
