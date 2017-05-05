9c3a8311eb47dec2d7a831fc84e6db6a9051b48b
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-05 10:58:30 +0300

    two versions of computation expression

--- Fourth_semestr/Homework_7/CalculationsWithRounding(Workflow)/CalculationsWithRounding(Workflow)/Program.fs
+++ Fourth_semestr/Homework_7/CalculationsWithRounding(Workflow)/CalculationsWithRounding(Workflow)/Program.fs
@@ -1,13 +1,37 @@
 ﻿module Program
 
-type MaybeBuid () =
-    member this.Bind(x, f) =
-        match x with
-        | None -> None
-        | Some a -> f a
-    member this.Return(x) = Some x
+type RoundBuilder(r:int) =
+    member this.Bind(x : float, f) = f <| System.Math.Round (x, r)
+    member this.Return(x : float) = System.Math.Round (x, r)
+          
+type RoundingBuilder() =
+    member this.Bind((x:float, r:int), f) = f <| System.Math.Round (x, r)
+    member this.Return(x:float, r:int) = System.Math.Round (x, r)
 
-let maybe = new MaybeBuid()
+let rounding = new RoundingBuilder()
+
+
+
+let compute x y r func =
+    rounding {
+        let! x' = (x, r)
+        let! y' = (y, r)
+        return (func x' y', r)
+        }
+
+let divide x y =
+    match y with
+    | 0.0 -> None
+    | _ -> Some (x / y)
+
+let round r = new RoundBuilder(r)
+
+let calculate x y func =
+    round 3 {
+        let! x' = x
+        let! y' = y
+        return (func x' y')
+        }
 
 //    rounding 3 {
 //        let! a = 2.0 / 12.0
@@ -17,5 +41,7 @@ let maybe = new MaybeBuid()
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let b = compute (2.0 / 12.0) 3.5 3 (fun x y -> x / y)
+    let c = calculate (2.0 / 12.0) 3.5 (fun x y -> x / y)
+    //    printfn "%A" argv
     0 // возвращение целочисленного кода выхода
