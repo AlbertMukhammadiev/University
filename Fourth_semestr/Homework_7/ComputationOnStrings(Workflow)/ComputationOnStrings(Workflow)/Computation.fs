c01cf43c81d7fb365027d95eabafda6b7299b4ce
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-06 20:57:32 +0300

    Workflow, performing calculations with numbers specified in the form of strings

---
+++ Fourth_semestr/Homework_7/ComputationOnStrings(Workflow)/ComputationOnStrings(Workflow)/Computation.fs
@@ -0,0 +1,38 @@
+﻿module Computation
+
+type MyInt =
+    | Value of int
+    | Invalid
+    static member (+) (x, y) =
+        match x with
+        | Invalid -> Invalid
+        | Value a -> match y with
+                     | Invalid -> Invalid
+                     | Value b -> Value <| a + b
+    static member (*) (x, y) =
+        match x with
+        | Invalid -> Invalid
+        | Value a -> match y with
+                     | Invalid -> Invalid
+                     | Value b -> Value <| a * b
+    static member (/) (x, y) =
+        match x with
+        | Invalid -> Invalid
+        | Value a -> match y with
+                     | Invalid -> Invalid
+                     | Value b -> Value <| a / b
+    static member (-) (x, y) =
+        match x with
+        | Invalid -> Invalid
+        | Value a -> match y with
+                     | Invalid -> Invalid
+                     | Value b -> Value <| a - b
+    static member toInt x =
+        try
+            Value << int <| x
+        with
+        | :? System.FormatException as ex -> Invalid
+
+type ConvertingBuilder() =
+    member this.Bind(x:string, f) = f << MyInt.toInt <| x
+    member this.Return(x) = x
