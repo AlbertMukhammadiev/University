9780bcde012fd4c21ede198a908254496dc270c0
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-06 15:09:03 +0300

    computation expression was corrected and tests were added

---
+++ Fourth_semestr/Homework_7/CalculationsWithRounding(Workflow)/CalculationsWithRounding(Workflow)/Tests.fs
@@ -0,0 +1,39 @@
+﻿module Tests
+
+open Rounding
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``result of expression rounded to thousandths`` () =
+    let rounding r = new RoundingBuilder(r)
+    let calculate =
+        rounding 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+            }
+    calculate |> should equal 0.048
+    
+[<Test>]
+let ``result of expression rounded to hundredths`` () =
+    let rounding r = new RoundingBuilder(r)
+    let calculate =
+        rounding 2 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+            }
+    calculate |> should equal 0.05
+
+[<Test>]
+let ``result of expression rounded to tenths`` () =
+    let rounding r = new RoundingBuilder(r)
+    let calculate =
+        rounding 1 {
+            let! a = 43.25 + 4.33
+            let! b = 2.0 / 12.0
+            let! c = 3.5
+            return a + b + c
+            }
+    calculate |> should equal 51.3
