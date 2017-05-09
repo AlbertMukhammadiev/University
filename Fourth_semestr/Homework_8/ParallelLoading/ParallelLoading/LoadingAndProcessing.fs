eba832ce9d436910d1c0e0c671af2d1250a642af
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-09 20:03:09 +0300

    The function searches references in html, loads web pages and writes the number of characters in them

---
+++ Fourth_semestr/Homework_8/ParallelLoading/ParallelLoading/LoadingAndProcessing.fs
@@ -0,0 +1,47 @@
+﻿module LoadingAndProcessing
+
+open System.Net
+open System.IO
+open System.Threading
+open System.Text.RegularExpressions
+
+let func url =
+    let fetchAsync url =
+        async {
+            do printfn "Creating request for %s..." url
+            let request = WebRequest.Create(url)
+            use! response = request.AsyncGetResponse()
+            do printfn "Getting response stream for %s..." url
+            use stream = response.GetResponseStream()
+            do printfn "Reading response for %s..." url
+            use reader = new StreamReader(stream)
+            let html = reader.ReadToEnd()
+            do printfn "Read %d characters for %s..." html.Length url
+        }
+
+    let references (url:string) =
+        let request = WebRequest.Create(url)
+        let response = request.GetResponse()
+        let stream = response.GetResponseStream()
+        let reader = new StreamReader(stream)
+        let html = reader.ReadToEnd()    
+        let matches = Regex.Matches(html, "(http:\/\/\S+)")
+        let rec ls i acc =
+            if i = 0 then
+                acc
+            else
+                let str = (matches.Item i).ToString()
+                let href = str.Remove <| str.IndexOf '\"'
+                ls (i - 1) (href :: acc)
+        ls (matches.Count - 1) []    
+
+    let load (urls:list<string>) =
+        let a = Seq.init urls.Length (fun i -> fetchAsync <| urls.Item i)
+        ignore << Async.RunSynchronously << Async.Parallel <| a
+
+    load <| references url
+
+[<EntryPoint>]
+let main argv = 
+    func "https://translate.google.com"
+    0 // возвращение целочисленного кода выхода
