2365e3b2ae52d333d0a8c8315d594045c4ee393f
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-12 11:16:11 +0300

    hash table

--- Fourth_semestr/test/test/Program.fs
+++ Fourth_semestr/test/test/Program.fs
@@ -2,11 +2,22 @@
 
 open PrintSquare
 open Sequence
-
+open HashTable
 
 [<EntryPoint>]
 let main argv = 
-    //printfn "%A" argv
-    print 10
-    Seq.iter (fun x -> printf "%A" x) <| Seq.take 100 sequence
+    let table1 = new HashTable(String.length)
+    table1.Add("Jacob")
+    table1.Add("William")
+    table1.Add("Christopher")
+    table1.Add("Ryan")
+    table1.Add("David")
+    table1.Add("Alexander")
+    table1.Add("James")
+    table1.Add("Jonathan")
+    table1.Add("Jose")
+    table1.Add("Kevin")
+    table1.Add("Luke")
+    table1.Add("Cameron")
+    table1.Print()
     0 // возвращение целочисленного кода выхода
