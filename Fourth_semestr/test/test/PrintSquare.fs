6cb5a70ccce37fc7f5719070224d9617f8537d9a
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-13 14:12:43 +0300

    Minor changes and bug(where n = 1 or 0) fixes

--- Fourth_semestr/test/test/PrintSquare.fs
+++ Fourth_semestr/test/test/PrintSquare.fs
@@ -1,24 +1,6 @@
 ﻿module PrintSquare
 
-//    let rec printTR n list =
-//        let length = List.length list
-//        match n with
-//        | length -> List.iter (fun x -> printfn "%c" x) list
-//                    printTR (n - 1) (list)
-//
-//        |  -> printfn "%c" '*'
-//               Seq.iter (fun x -> printfn "%c" x) <| Seq.take (length - 2) list
-//               printfn "%c" '*'
-//               printTR (n - 1) (list)
-
-//    let rec create list n acc =
-//        if acc = n then list
-//        else
-//            create ('*' :: list) n (acc + 1)
-
 let print n =
-    let seq n = Seq.init n (fun x -> '*')
-
     let rec printTR n seq =
         let length = Seq.length seq
         if length = n then
@@ -32,7 +14,10 @@ let print n =
             printf "%c" '*'
             Seq.iter (fun x -> printf "%c" ' ') <| Seq.take (length - 2) seq
             printfn "%c" '*'
-            printTR (n - 1) (seq)      
+            printTR (n - 1) (seq)    
 
-    let stars = seq n
-    printTR n stars 
+    if n < 2 then printfn "%c" '*'
+    else
+        let seq n = Seq.init n (fun x -> '*')
+        let stars = seq n
+        printTR n stars 
--- Fourth_semestr/test/test/Program.fs
+++ Fourth_semestr/test/test/Program.fs
@@ -6,18 +6,5 @@ open HashTable
 
 [<EntryPoint>]
 let main argv = 
-    let table1 = new HashTable(String.length)
-    table1.Add("Jacob")
-    table1.Add("William")
-    table1.Add("Christopher")
-    table1.Add("Ryan")
-    table1.Add("David")
-    table1.Add("Alexander")
-    table1.Add("James")
-    table1.Add("Jonathan")
-    table1.Add("Jose")
-    table1.Add("Kevin")
-    table1.Add("Luke")
-    table1.Add("Cameron")
-    table1.Print()
+    print 10
     0 // возвращение целочисленного кода выхода
