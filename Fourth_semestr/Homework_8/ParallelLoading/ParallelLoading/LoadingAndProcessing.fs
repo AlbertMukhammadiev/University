4499390f726216b160f01b9bb725f074212dfaa8
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-19 15:17:04 +0300

    tests

--- Fourth_semestr/Homework_8/ParallelLoading/ParallelLoading/LoadingAndProcessing.fs
+++ Fourth_semestr/Homework_8/ParallelLoading/ParallelLoading/LoadingAndProcessing.fs
@@ -5,36 +5,50 @@ open System.IO
 open System.Threading
 open System.Text.RegularExpressions
 
-let func url =
-    let fetchAsync url =
-        async {
-            do printfn "Creating request for %s..." url
-            let request = WebRequest.Create(url)
-            use! response = request.AsyncGetResponse()
-            do printfn "Getting response stream for %s..." url
-            use stream = response.GetResponseStream()
-            do printfn "Reading response for %s..." url
-            use reader = new StreamReader(stream)
-            let html = reader.ReadToEnd()
-            do printfn "Read %d characters for %s..." html.Length url
-        }
+let references (url:string) =
+    let request = WebRequest.Create(url)
+    let response = request.GetResponse()
+    let stream = response.GetResponseStream()
+    let reader = new StreamReader(stream)
+    let html = reader.ReadToEnd()    
+    let matches = Regex.Matches(html, "(http:\/\/\S+)")
+    let rec ls i acc =
+        if i = 0 then
+            acc
+        else
+            let str = (matches.Item i).ToString()
+            let href = str.Remove <| str.IndexOf '\"'
+            ls (i - 1) (href :: acc)
+    
+    ls (matches.Count - 1) []    
 
-    let references (url:string) =
+let fetchAsync url =
+    async {
+        do printfn "Creating request for %s..." url
         let request = WebRequest.Create(url)
-        let response = request.GetResponse()
-        let stream = response.GetResponseStream()
-        let reader = new StreamReader(stream)
-        let html = reader.ReadToEnd()    
-        let matches = Regex.Matches(html, "(http:\/\/\S+)")
-        let rec ls i acc =
-            if i = 0 then
-                acc
-            else
-                let str = (matches.Item i).ToString()
-                let href = str.Remove <| str.IndexOf '\"'
-                ls (i - 1) (href :: acc)
-        ls (matches.Count - 1) []    
+        use! response = request.AsyncGetResponse()
+        do printfn "Getting response stream for %s..." url
+        use stream = response.GetResponseStream()
+        do printfn "Reading response for %s..." url
+        use reader = new StreamReader(stream)
+        let html = reader.ReadToEnd()
+        do printfn "Read %d characters for %s..." html.Length url
+    }
 
+let fetchAsync2 (url:string) =
+    async {
+        //do printfn "Creating request for %s..." url
+        let request = WebRequest.Create(url)
+        use! response = request.AsyncGetResponse()
+        //do printfn "Getting response stream for %s..." url
+        use stream = response.GetResponseStream()
+        //do printfn "Reading response for %s..." url
+        use reader = new StreamReader(stream)
+        let html = reader.ReadToEnd()
+        return html.Length
+    }
+
+let func url =
     let load (urls:list<string>) =
         let a = Seq.init urls.Length (fun i -> fetchAsync <| urls.Item i)
         ignore << Async.RunSynchronously << Async.Parallel <| a
@@ -42,6 +56,6 @@ let func url =
     load <| references url
 
 [<EntryPoint>]
-let main argv = 
+let main (string) =
     func "https://translate.google.com"
-    0 // возвращение целочисленного кода выхода
+    0
---
+++ Fourth_semestr/Homework_8/ParallelLoading/ParallelLoading/Tests.fs
@@ -0,0 +1,10 @@
+﻿module Tests
+//
+//open LoadingAndProcessing
+//open NUnit.Framework
+//open FsUnit
+//
+//[<Test>]
+//let ``correctness of references`` () =
+//    let ls = references "https://translate.google.com"
+//    List.forall (fun x -> x.ToString().StartsWith("http")) ls |> should equal true
