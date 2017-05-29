e6662cd75a01c50e1430414ab918643b27eb2267
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-29 12:17:50 +0300

    initial commit

---
+++ Fourth_semestr/Homework_5/Beta-reduction(NormalStrategy)/Beta-reduction(NormalStrategy)/Reducer.fs
@@ -0,0 +1,21 @@
+﻿module Reducer
+
+type Term =
+    | Variable of char
+    | Abstraction of char * Term
+    | Application of Term * Term
+
+let rec print term =
+    match term with
+    | Variable x -> printf "%A" x
+    | Abstraction (x, t) -> printf "%A" ("L" + x + ".")
+                            print term
+    | Application (t1, t2) -> print term
+                              print term
+
+let substitute y term =
+    match term with
+    | Variable x -> printf "%A" x
+    | Abstraction (x, t) -> substitute t y
+    | Application (t1, t2) -> substitute t1 y
+                              substitute t2 y
