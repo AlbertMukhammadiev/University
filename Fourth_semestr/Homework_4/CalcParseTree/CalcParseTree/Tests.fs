6dccd51431f30cd3f9e8b1a7ba136a004d7e2bbc
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-31 15:35:56 +0300

    file renaming and changing type of operator

--- Fourth_semestr/Homework_4/CalcParseTree/CalcParseTree/Tests.fs
+++ Fourth_semestr/Homework_4/CalcParseTree/CalcParseTree/Tests.fs
@@ -8,32 +8,22 @@ open FsUnit
 let ``Calculating of ((7 + 3) * (5 - 2))`` () =
     let tree =
         Node (
-            '*',
-            Node ('+', Tip 7, Tip 3),
-            Node ('-', Tip 5, Tip 2)
+            Multiplication,
+            Node (Addition, Tip 7, Tip 3),
+            Node (Substraction, Tip 5, Tip 2)
             )
     calculate tree |> should equal 30
-
-[<Test>]
-let ``provoking of throw of exception`` () =
-    let tree =
-        Node (
-            'w',
-            Node ('+', Tip 7, Tip 3),
-            Node ('-', Tip 5, Tip 2)
-            )
-    (fun () -> calculate tree |> ignore) |> should (throwWithMessage "incorrect operator") typeof<System.Exception> 
 
 [<Test>]
 let ``Calculating of (5 * (10 - (30 + 2)))`` () =
     let tree =
         Node (
-            '*',
+            Multiplication,
             Tip 5,
             Node (
-                '-',
+                Substraction,
                 Tip 10,
-                Node ('+', Tip 30, Tip 2)
+                Node (Addition, Tip 30, Tip 2)
                 )
             )
     calculate tree |> should equal -110
