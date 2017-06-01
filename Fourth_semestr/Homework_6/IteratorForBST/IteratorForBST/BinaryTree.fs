f0862ed6e376c65652af96b277d2b443f16fc589
AlbertMukhammadiev <beehead02@gmail.com>
2017-06-01 12:10:10 +0300

    initial commit
    Functions for inserting, deleting and searching are ready

---
+++ Fourth_semestr/Homework_6/IteratorForBST/IteratorForBST/BinaryTree.fs
@@ -0,0 +1,58 @@
+﻿module BinaryTree
+
+type Tree<'a> =
+    | EmptyTree
+    | TreeNode of 'a * int * Tree<'a> * Tree<'a>
+
+type BinaryTree<'a> () =
+    let mutable root = EmptyTree
+
+    let rec insert value key tree =
+        match tree with
+            | EmptyTree -> TreeNode (value, key, EmptyTree, EmptyTree)
+            | TreeNode (v, k, left, right) when k > key -> TreeNode (v, k, insert value key left, right)
+            | TreeNode (v, k, left, right) when k < key -> TreeNode (v, k, left, insert value key right)
+            | _ -> tree
+
+    /// data from node with minimum key
+    let rec minimum node =
+        match node with
+        | TreeNode (v, k, EmptyTree, _) -> v, k
+        | TreeNode (v, k, left, _) -> minimum left
+
+    let rec remove key tree =
+        match tree with
+        | EmptyTree -> EmptyTree
+        | TreeNode (v, k, left, right) when key < k -> TreeNode (v, k, remove key left, right)
+        | TreeNode (v, k, left, right) when key > k -> TreeNode (v, k, left, remove key right)
+        | TreeNode (v, k, EmptyTree, right) when key = k -> right
+        | TreeNode (v, k, left, EmptyTree) when key = k -> left
+        | TreeNode (v, k, left, right) when key = k -> let value, key = minimum right
+                                                       let node = remove key right
+                                                       TreeNode (value, key, left, node)
+        | _ -> EmptyTree
+
+    member this.Insert value key = root <- insert value key root
+
+    member this.GetValue key =
+        let rec search key node =
+            match node with
+            | TreeNode (v, k, left, right) when key < k -> search key left
+            | TreeNode (v, k, left, right) when key > k -> search key right
+            | TreeNode (v, k, _, _) when key = k -> Some v
+            | _ -> None
+        search key root
+
+    member this.Remove key = root <- remove key root
+
+    member this.Print () =
+        let rec print tree =
+            match tree with
+            | EmptyTree -> ()
+            | TreeNode (v, k, left, right) -> print left            
+                                              printfn "%A" v
+                                              print right
+        print root
+
+
+    
---
+++ Fourth_semestr/Homework_6/IteratorForBST/IteratorForBST/Program.fs
@@ -0,0 +1,29 @@
+﻿module Program
+
+open BinaryTree
+
+let poemTree = 
+    let mutable tree = BinaryTree<string>()
+    do
+        tree.Insert "беда" 50
+        tree.Insert "опилки" 60
+        tree.Insert "не" 40
+        tree.Insert "голове" 55
+        tree.Insert "да" 70
+        tree.Insert "я" 30
+        tree.Insert "моей" 58
+        tree.Insert "если" 20
+        tree.Insert "в." 53
+        tree.Insert "в" 35
+        tree.Insert "да." 75
+        tree.Insert "да.." 80
+        tree.Insert "чешу" 33
+        tree.Insert "затылке" 37
+        tree.Remove 50
+    tree
+
+[<EntryPoint>]
+let main argv = 
+    let tree = poemTree
+    poemTree.Print ()
+    0 // возвращение целочисленного кода выхода
