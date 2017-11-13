53c1a08dd30e7f613c3fe47f2986f9d55bf78374
AlbertMukhammadiev <beehead02@gmail.com>
2017-11-13 18:28:13 +0300

    approximation of arbitrary polinomial was added

--- Fifth_semester/Numerical_Analysis/PolynomialOfBestApproximation/Approximate2.m
+++ Fifth_semester/Numerical_Analysis/PolynomialOfBestApproximation/Approximate2.m
@@ -1,23 +1,37 @@
-% builds the polinomial of best approximation on [0; 1]
-% for f(x) = x^n;
+% builds the polinomial of best approximation on [-1; 1]
+% for a polynomial of degree n
 % draws Chebyshev polinomial of n-th degree, approximating function
 % and alternance points;
-function [ ] = Approximate2( n )
+% arguments: array - polynomial coefficients
+function [ ] = Approximate2( array )
 numOfNodes = 101;
-dx = 1 / (numOfNodes - 1);
-x = 0 : dx : 1;
+% the boundaries of the interval [a,b]
+a = -1;
+b = 1;
+dx = 2 / (numOfNodes - 1);
+x = a : dx : b;
+n = length(array) - 1;
 max = 0;
 
 for i = 1 : numOfNodes
-    % function
-    fx(i) = x(i) ^ n;
+    xn(i) = x(i) ^ n;
+    
+    % (n - 1)-th tail of the given polinomial
+    tail(i) = 0;
+    for k = 1 : n
+        tail(i) = tail(i) + (x(i) ^ (k - 1)) * array(k);
+    end
+    
+    % the given polinomial
+    Pnx(i) = array(n + 1) * xn(i) + tail(i);
+    
     % Chebyshev polynomial
     Tnx(i) = cos (n * acos(x(i)));
     % approximating function
-    y(i) = fx(i) - Tnx(i) / 2 ^ (n - 1);
+    y(i) = (xn(i) - Tnx(i) / 2 ^ (n - 1)) * array(n + 1) + tail(i);
     
     % searching of a max for search of alternance points
-    norm = abs(fx(i) - y(i));
+    norm = abs(xn(i) - y(i));
     if norm > max
         max = norm;
     end
@@ -27,22 +41,32 @@ end
 k = 1;
 E = 1 / 100^4;
 for i = 1 : numOfNodes
-    if ((abs(fx(i) - y(i)) <= max + E) && (abs(fx(i) - y(i)) >= max - E))
-        alt(k) = dx * (i - 1);
+    if ((abs(xn(i) - y(i)) <= max + E) && (abs(xn(i) - y(i)) >= max - E))
+        alt(k) = a + dx * (i - 1);
         k = k + 1;
     end
 end
 
 nStr = int2str(n);
+PnStr = '';
+for k = n + 1 : -1 : 2
+    if (array(k)) ~= 0
+        PnStr = [PnStr int2str(array(k)) 'x^' int2str(k - 1) '+ '];
+    end
+end;
+PnStr = [PnStr int2str(array(1))];
 
 hold on;
-fPlot = plot(x, fx, 'DisplayName', strrep('f(x)=x^n', 'n', nStr));
+
+PnxPlot = plot(x, Pnx, 'DisplayName', PnStr);
+xnPlot = plot(x, xn, 'DisplayName', strrep('f(x)=x^n', 'n', nStr));
 TnPlot = plot(x, Tnx, 'DisplayName', strrep('T_n(x)', 'n', nStr));
 yPlot =  plot(x, y, 'DisplayName', 'approximating function');
 altPlot = plot(alt, 0, 'o', 'DisplayName', 'alternance point');
 hold off;
 
-set(fPlot, 'LineWidth', 1.5);
+set(PnxPlot, 'LineWidth', 1.5);
+set(xnPlot, 'LineWidth', 1.5);
 set(TnPlot, 'LineWidth', 1.5);
 set(yPlot, 'LineWidth', 1.5);
 set(altPlot, 'LineWidth', 2);
