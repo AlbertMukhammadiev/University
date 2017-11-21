7b555f838d0b4f47895cd8b49fea5ea14da59d9f
AlbertMukhammadiev <beehead02@gmail.com>
2017-11-21 10:32:53 +0300

    drawing polinomial was added

--- Fifth_semester/Numerical_Analysis/Newton&LagrangePolynomials/Interpolation_LagrangeForm.m
+++ Fifth_semester/Numerical_Analysis/Newton&LagrangePolynomials/Interpolation_LagrangeForm.m
@@ -4,25 +4,28 @@ function [ ] = Interpolation_LagrangeForm( nodesNum )
 a = 0; b = 1;
 
 % function
-x1 = 0 : 0.001 : 1;
-fx = f(x1);
+x = 0 : 0.001 : 1;
+fx = f(x);
 
 % interpolation
 dx = (b - a) / (nodesNum - 1);
-x2 = 0 : dx : 1;
-for i = 1 : length(x2)
-    px(i) = LagrangePolinomial(x2, f(x2), x2(i));
+nodes = 0 : dx : 1;
+for i = 1 : length(x)
+    px(i) = LagrangePolinomial(nodes, f(nodes), x(i));
 end
 
 h = figure;
 title('Interpolation polynomial in the Lagrange form');
 hold on;
 grid on;
-pxPlot = plot(x2, px, '-o', 'DisplayName',...
+pxPlot = plot(x, px, 'DisplayName',...
                 strrep('P_{n}(x)', 'n', int2str(nodesNum - 1)));
-fxPlot = plot(x1, fx, 'DisplayName', 'sin^5(5x)');
+fxPlot = plot(x, fx, 'DisplayName', 'sin^5(5x)');
+nodesPlot = plot(nodes, f(nodes), 'o', 'DisplayName',...
+                'interpolation nodes');
 set(pxPlot, 'LineWidth', 1.5);
 set(fxPlot, 'LineWidth', 1.5);
+set(nodesPlot, 'LineWidth', 1.5);
 legend('show');
 print(h, '-dpng', '-r300', 'graph1');
 end
--- Fifth_semester/Numerical_Analysis/Newton&LagrangePolynomials/Interpolation_NewtonForm.m
+++ Fifth_semester/Numerical_Analysis/Newton&LagrangePolynomials/Interpolation_NewtonForm.m
@@ -4,26 +4,29 @@ function [ ] = Interpolation_NewtonForm( nodesNum )
 a = 0; b = 1;
 
 % function
-x1 = 0 : 0.001 : 1;
-fx = f(x1);
+x = 0 : 0.001 : 1;
+fx = f(x);
 
 % interpolation
 dx = (b - a) / (nodesNum - 1);
-x2 = 0 : dx : 1;
-Y = DividedDifferences(x2, f(x2), length(x2));
-for i = 1 : length(x2)
-    px(i) = NewtonPolinomial(x2, Y, x2(i));
+nodes = 0 : dx : 1;
+Y = DividedDifferences(nodes, f(nodes), length(nodes));
+for i = 1 : length(x)
+    px(i) = NewtonPolinomial(nodes, Y, x(i));
 end
 
 h = figure;
 title('Interpolation polynomial in the Newton form');
 hold on;
 grid on;
-pxPlot = plot(x2, px, '-o', 'DisplayName',...
+pxPlot = plot(x, px, 'DisplayName',...
                 strrep('P_{n}(x)', 'n', int2str(nodesNum - 1)));
-fxPlot = plot(x1, fx, 'DisplayName', 'sin^5(5x)');
+fxPlot = plot(x, fx, 'DisplayName', 'sin^5(5x)');
+nodesPlot = plot(nodes, f(nodes), 'o', 'DisplayName',...
+                'interpolation nodes');
 set(pxPlot, 'LineWidth', 1.5);
 set(fxPlot, 'LineWidth', 1.5);
+set(nodesPlot, 'LineWidth', 1.5);
 legend('show');
 print(h, '-dpng', '-r300', 'graph2');
 end
