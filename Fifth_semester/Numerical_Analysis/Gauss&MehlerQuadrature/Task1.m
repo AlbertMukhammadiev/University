92f7d895bc95d43d252ff381ee439dc9b3bbc414
AlbertMukhammadiev <beehead02@gmail.com>
2017-12-20 10:01:16 +0300

    computing of theoretical errors was fixed

--- Fifth_semester/Numerical_Analysis/Gauss&MehlerQuadrature/Task1.m
+++ Fifth_semester/Numerical_Analysis/Gauss&MehlerQuadrature/Task1.m
@@ -2,47 +2,61 @@
 function [ ] = Task1( func )
     a = 0;
     b = 0.4;
-    n = 5;
+    n0 = 4;
+    n = 13;
+    xs = n0 : 1 : n;
     
     syms x;
-    I = double(vpa(int(func, x, a, b), 15));
-    
-    % search of max difference in [a, b]
-    maxDiff = -inf;
-    df = diff(func, x, 2 * n);
-    for i = 0 : 20
-        x = a + i * 0.01;
-        temp = eval(df);
-        if (temp > maxDiff)
-            maxDiff = temp;
-        end
-    end
-    
-    error = double((b - a)^(2 * i + 1) * (factorial(i))^4 /...
-            (2 * i + 1) / (fix(factorial(2 * n)))^3 * maxDiff);
-    
-    for i = 1 : n    
+    I = double(vpa(int(func, sym('x'), a, b), 15));
+
+    for i = n0 : n
+        j = i - 3;
         S = GaussLegendreQuadrature(func, a, b, i);
-        actualError = I - S;
-        integrals(i) = I;
-        Sums(i) = S;
-        actualErrors(i) = actualError;
-        theoreticalErrors(i) = error;
-    end
+        integrals(j) = I;
+        Sums(j) = S;
+        actualErrors(j) = I - S;
+        
+        % search of max difference in [a, b]
+        maxDiff = -inf;
+        df = diff(func, sym('x'), 2 * j);
+        x = a + 0.1 ;
+        maxDiff = eval(df);
+%         for j = 0 : 20
+%             x = a + i * 0.01;
+%             temp = eval(df);
+%             if (temp > maxDiff)
+%                 maxDiff = temp;
+%             end
+%         end
     
-    integrals = num2cell(integrals);
-    Sums = num2cell(Sums);
-    actualErrors = num2cell(actualErrors);
-    theoreticalErrors = num2cell(theoreticalErrors);
-    data = [integrals; Sums; actualErrors; theoreticalErrors];
-
-    rowNames = {'exact value', 'approximation', 'actual error',...
-                'theoretical error'};
-    columnNames = {'n = 1', 'n = 2', 'n = 3', 'n = 4', 'n = 5'};
-    f = figure('Position', [500 500 570 100]);
-    t = uitable( f, 'Data', data, 'ColumnName',columnNames,...
-                'RowName',rowNames,'Position', [0 0 565 95]);
-    print(f, '-dpng', '-r300', 'table_GaussLegendre');
-
+        error(j) = double((b - a)^(2 * j + 1) * (factorial(j))^4 /...
+                (2 * j + 1) / (fix(factorial(2 * j)))^3 * maxDiff);
+        theoreticalErrors(j) = error(j);
+    end
+%     
+%     integrals = num2cell(integrals);
+%     Sums = num2cell(Sums);
+%     actualErrors = num2cell(actualErrors);
+%     theoreticalErrors = num2cell(theoreticalErrors);
+%     data = [integrals; Sums; actualErrors; theoreticalErrors];
+% 
+%     rowNames = {'exact value', 'approximation', 'actual error',...
+%                 'theoretical error'};
+%     columnNames = {'n = 1', 'n = 2', 'n = 3', 'n = 4', 'n = 5'};
+%     f = figure('Position', [500 500 570 100]);
+%     t = uitable( f, 'Data', data, 'ColumnName',columnNames,...
+%                 'RowName',rowNames,'Position', [0 0 565 95]);
+%     print(f, '-dpng', '-r300', 'table_GaussLegendre');
+    f = figure;
+    title('Gauss-Legendre Quadrature');
+    hold on;
+    grid on;
+    actErrPlot = semilogy(xs, actualErrors, 'DisplayName', 'Actual errors');
+    theorErrPlot = semilogy(xs, theoreticalErrors,...
+                            'DisplayName', 'Theoretical errors');
+    set(actErrPlot, 'LineWidth', 1.5);
+    set(theorErrPlot, 'LineWidth', 1.5);
+    legend('show');
+    
 end
 
--- Fifth_semester/Numerical_Analysis/Gauss&MehlerQuadrature/Task2.m
+++ Fifth_semester/Numerical_Analysis/Gauss&MehlerQuadrature/Task2.m
@@ -4,22 +4,24 @@ function [ ] = Task2( func )
     xs = 1 : 1 : n;
     a = -1;
     b = 1;
-    syms x;
-    I = vpa(int(func / sym('sqrt(1 - x^2)'), x, a, b), 15);
+    format long;
+    I = vpa(int(func / sym('sqrt(1 - x^2)'), sym('x'), a, b), 15);
     
-    % search of max difference in [a, b]
-    maxDiff = -inf;
-    df = diff(func, x, 2 * n);
-    for i = 0 : 20
-        x = a + i * 0.01;
-        temp = eval(df);
-        if (temp > maxDiff)
-            maxDiff = temp;
-        end
-    end
-    
-    maxError = pi / factorial(2 * n) / 2^(2 * n - 1) * maxDiff;
     for k = 1 : n
+        % search of max difference in [a, b]
+        maxDiff(k) = inf;
+        df = diff(func, sym('x'), 2 * k);
+        x = a + 0.5 ;
+        maxDiff(k) = eval(df);
+%         for i = 0 : 2 * n
+%             x = a + i * 0.1;
+%             temp = eval(df);
+%             if (temp < maxDiff(k))
+%                 maxDiff(k) = temp;
+%             end
+%         end
+        
+        maxError(k) = pi * maxDiff(k) / factorial(2 * k) / (2^(2 * k - 1));
         Sums(k) = MehlerQuadrature(func, k);
         actualErrors(k) = I - Sums(k);
     end
@@ -31,10 +33,10 @@ function [ ] = Task2( func )
     hold on;
     grid on;
     actErrPlot = semilogy(xs, actualErrors, 'DisplayName', 'Actual errors');
-    %theorErrPlot = semilogy(xs, ones(size(xs)) * maxError,...
-     %                       'DisplayName', 'Theoretical errors');
+    theorErrPlot = semilogy(xs, maxError,...
+                            'DisplayName', 'Theoretical errors');
     set(actErrPlot, 'LineWidth', 1.5);
-    %set(theorErrPlot, 'LineWidth', 1.5);
+    set(theorErrPlot, 'LineWidth', 1.5);
     legend('show');
-    print(f, '-dpng', '-r300', 'Mehler2');
+    %print(f, '-dpng', '-r300', 'Mehler2');
 end
