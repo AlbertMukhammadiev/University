56f58534b193e626fed9ae9ef4a7b9ec7a0b9d09
AlbertMukhammadiev <beehead02@gmail.com>
2017-12-22 02:16:49 +0300

    the previous method was erroneous

--- Fifth_semester/Numerical_Analysis/RiemannSumForPeriodicFunction/RiemannSum.m
+++ Fifth_semester/Numerical_Analysis/RiemannSumForPeriodicFunction/RiemannSum.m
@@ -1,21 +1,19 @@
-function [ S, R ] = RiemannSum( func, n, T )
-S = 2 * int(func, sym('x'), 0, T) / T;
-
-for k = 1 : n
-    t = 2 * pi * k * sym('x') / T;
-    a_k = 2 * int(func * cos(t), sym('x'), 0, T) / T;
-    b_k = 2 * int(func * sin(t), sym('x'), 0, T) / T;
-    A_k = sqrt(a_k ^ 2 + b_k ^ 2);
-    S = S + A_k;
-    
-    t = 2 * pi * (-k) * sym('x') / T;
-    a_k = 2 * int(func * cos(t), sym('x'), 0, T) / T;
-    b_k = 2 * int(func * sin(t), sym('x'), 0, T) / T;
-    A_k = sqrt(a_k ^ 2 + b_k ^ 2);
-    S = S + A_k;
-    
-    R = pi * A_k;
-end
-    
-S = vpa(S, 15);
+% calculates an approximate value of the integral using Midpoint rule
+% arguments:
+%   func - function
+%   a - left boarder
+%   n - number of nodes
+%   T - period
+function [ S ] = RiemannSum( func, a, n, T )
+    f = symfun(sym(func), sym('x'));
+    b = a + T;
+    dx = (b - a) / (n - 1);
+    x = a;
+    S = sym(0);
+    for i = 0 : n - 2
+        middle = (x + x + dx) / 2;
+        S = S + f(middle) * dx;
+        x = x + dx;
+    end
+    S = eval(S);
 end
